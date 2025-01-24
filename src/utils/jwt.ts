77fb7bd378057db72e127bed58eebe4411bd4f4d
Oleksandr <oleksandr.vatamaniuk@sombrainc.com>
2025-01-24 19:45:27 +0200

    Create dockerfile

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,4 +1,4 @@
-import {sign, verify} from "jsonwebtoken";
+import {Secret, sign, SignOptions, verify} from "jsonwebtoken";
 import {Response} from "express";
 import * as process from "process";
 import  { IUser } from "../models/User";
@@ -7,24 +7,24 @@ export interface JWTUserPayload {
     userId: string
 }
 
-export interface RefreshTokenUserPayload extends JWTUserPayload{
-    tokenVersion: string
-}
+// export interface RefreshTokenUserPayload extends JWTUserPayload{
+//     tokenVersion: string
+// }
 
 export const createAccessToken = (user: IUser): string => {
-    return sign({userId: user.id}, process.env.ACCESS_TOKEN_SECRET!, {
-        expiresIn: process.env.ACCESS_TOKEN_LIFETIME!
-    })
+    return sign({userId: user.id}, process.env.ACCESS_TOKEN_SECRET as Secret, {
+        expiresIn: process.env.ACCESS_TOKEN_LIFETIME as string
+    } as SignOptions)
 }
 
 export const createRefreshToken = (user: IUser): string => {
-    return sign({userId: user.id, tokenVersion: user.tokenVersion}, process.env.REFRESH_TOKEN_SECRET!, {
-        expiresIn: process.env.REFRESH_TOKEN_LIFETIME!
-    })
+    return sign({userId: user.id, tokenVersion: user.tokenVersion}, process.env.REFRESH_TOKEN_SECRET as Secret, {
+        expiresIn: process.env.REFRESH_TOKEN_LIFETIME as string
+    } as SignOptions)
 }
 
-export const isAccessTokenValid = ( token: string ) => verify(token, process.env.ACCESS_TOKEN_SECRET!);
-export const isRefreshTokenValid = ( token: string ) => verify(token, process.env.REFRESH_TOKEN_SECRET!);
+export const isAccessTokenValid = ( token: string ) => verify(token, process.env.ACCESS_TOKEN_SECRET as string );
+export const isRefreshTokenValid = ( token: string ) => verify(token, process.env.REFRESH_TOKEN_SECRET as string);
 
 export const sendRefreshToken = async (res: Response, user: IUser) => {
     await user.$inc('tokenVersion', 1).save();
